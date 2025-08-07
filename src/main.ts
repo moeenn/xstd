bf61580bc58811b02271820ca7bd8ca7675e96bd
moeenn <moeen.v8@gmail.com>
2025-08-08 03:11:06 +0500

    updated main

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,8 @@
-import { HttpClient, HttpRequest } from "./core/HttpClient.js"
-import { Results, type Result } from "./core/Result.js"
-import { File } from "./node/File.js"
-import { type ReadableStream } from "node:stream/web"
+import { fmt } from "./node/Fmt.js"
+import { Options, type Option } from "./core/Option.js"
 
-async function run(): Promise<Result<unknown>> {
-    const rawURL =
-        "https://cdni.nastypornpics.com/1280/5/98/83232692/83232692_013_ac54.jpg"
-
-    const url = Results.of(() => new URL(rawURL))
-    if (!url.isValid) {
-        return url
-    }
-
-    const req = new HttpRequest(url.value).setTimeout(100_000)
-    const client = new HttpClient()
-
-    const res = await client.stream(req)
-    if (!res.isValid) {
-        return res
-    }
-
-    console.log("name :: ", res.value.constructor.name)
-    const file = new File("/home/moeenn/Downloads/image.jpg")
-    const writeResult = await file.writeFromStream(res.value as ReadableStream)
-    console.log(writeResult)
-
-    return Results.nil()
-}
-
-async function main() {
-    const output = await run()
-    if (!output.isValid) {
-        console.error("error: " + output.error)
-        process.exit(1)
-    }
+function main() {
+    fmt.print("hello world")
 }
 
 main()
